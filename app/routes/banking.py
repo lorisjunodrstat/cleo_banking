d7da2d05a75bcc6cdf00292452d2514514f2764b
LorisjunodUnine <loris.junod@Unine.ch>
2026-09-13 18:59:28 +0200

    correction fonction de comptabilisationd de vente

--- app/routes/banking.py
+++ app/routes/banking.py
@@ -1,7 +1,7 @@
 from typing import List, Dict, Optional, Tuple, TypedDict, Any
 import logging
 from types import SimpleNamespace
-from flask import Blueprint, render_template, request, redirect, url_for, flash, jsonify, make_response, current_app, g, session, abort, send_file, Response, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash, jsonify, make_response, current_app, g, session, abort, send_file, Response, url_for, _app_ctx_stack
 from urllib.parse import urlencode
 from flask_login import login_required, current_user
 from decimal import Decimal, InvalidOperation
@@ -45,15 +45,12 @@ from reportlab.lib.units import inch
 from reportlab.lib import colors
 from reportlab.lib.pagesizes import A4
 from ..utils.pdf_salaire import generer_pdf_salaire
-# --- DÉBUT DES AJOUTS (8 lignes) ---
-from flask import _app_ctx_stack
+
 
 
 
 # Création du blueprint
 bp = Blueprint('banking', __name__)
-from flask import request, session, g, current_user, url_for
-from urllib.parse import urlencode
 
 @bp.app_context_processor
 def inject_pos_magasin_context():
